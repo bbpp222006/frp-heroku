66b365ac02fcbbef21b230e9692c9bb9a4b8030a
bbpp222006 <34976166+bbpp222006@users.noreply.github.com>
2020-10-09 15:58:39 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,14 +5,11 @@ cd /frps
 cat <<-EOF > /frps/frps.ini
 [common]
 
-bind_port = $PORT
-kcp_bind_port = 9527
-dashboard_port = 9530
+bind_port = 80
+dashboard_port = 443
 dashboard_user = admin
-dashboard_pwd = $DASH
-vhost_http_port = 9528
-vhost_https_port = 9529
-token = $TOKEN
+dashboard_pwd = ${dash_password}
+token = ${token}
 
 
 EOF
